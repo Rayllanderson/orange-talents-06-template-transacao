32b66444fd335e811d9eb141f27bd3c7154b4dc6
Rayllanderson <rayllanderson@gmail.com>
2021-07-22 09:27:54 -0300

    Setup do projeto

---
+++ src/test/java/br/com/zupacademy/rayllanderson/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.rayllanderson.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
